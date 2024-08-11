bfa9b00b5890bdefaf9eed9fea647bf5a4c222df
YashPratap19 <yashpratap19.singh@gmail.com>
2024-08-11 22:33:25 +0530

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Explore a Diverse Array of Culinary Delights with Our Extensive Menu, Featuring Handpicked Dishes and Gourmet Selections Delivered Straight to Your Doorstep, Ensuring Every Meal is an Unforgettable Experience</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
